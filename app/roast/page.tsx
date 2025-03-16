b7152517c1a402f6c54e6713d5d67aeaeec8926d
RChaubey16 <ruturajchaubey16@gmail.com>
2025-03-16 09:13:58 +0530

    feat: Add roast route to display user's roast content.

---
+++ app/roast/page.tsx
@@ -0,0 +1,39 @@
+export default function RoastPage() {
+  return (
+    <main className="container-padding h-[100vh] flex justify-center">
+      <div className="max-w-[700px] w-full flex flex-col items-center my-8 overflow-scroll overflow-x-hidden">
+        <p className="text-base font-medium text-silver text-left">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          commodo metus vel ex finibus, at vehicula est hendrerit. Proin
+          fringilla risus porta molestie congue. Fusce viverra vehicula
+          vestibulum. Fusce eleifend vel sapien et molestie. Donec id suscipit
+          purus, nec cursus ligula. Nunc tristique viverra erat eget pretium.
+          Aliquam erat volutpat. Aenean enim nisi, maximus vitae odio dignissim,
+          interdum tempus lorem. Nulla molestie lacinia ex, vel venenatis magna
+          ornare vel. Mauris quis nisl est. Aenean elementum sem vitae mi
+          fermentum dignissim. Fusce ex eros, facilisis ut feugiat vitae,
+          accumsan ac est. Aenean tristique sapien hendrerit, scelerisque magna
+          a, pretium dui. Duis et egestas diam. Nam cursus nulla diam, in tempor
+          elit interdum ut. Maecenas vitae molestie est. Vivamus volutpat
+          sollicitudin ligula at pretium. Aliquam vel lorem ligula. Nulla eget
+          faucibus lacus, at congue orci. Nam euismod vestibulum ultrices. Nulla
+          dignissim semper enim quis ullamcorper. Interdum et malesuada fames ac
+          ante ipsum primis in faucibus. Praesent viverra dignissim feugiat.
+          Etiam augue orci, auctor ut libero at, maximus dapibus dolor. Ut
+          eleifend diam nulla, non vehicula felis mattis a. Maecenas aliquam
+          tempus arcu finibus convallis. Quisque nisi augue, eleifend quis
+          ligula quis, pretium pretium quam. Pellentesque ipsum ligula, rhoncus
+          nec purus vel, blandit tincidunt lorem. Mauris at rhoncus sem. Donec
+          tristique neque quis pharetra auctor. In lobortis ut nulla vitae
+          venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Quisque porta scelerisque justo non faucibus. Aenean dictum at ligula
+          non elementum. Duis vitae erat ornare, sodales mauris quis, molestie
+          orci. Donec odio quam, efficitur et tempor sit amet, viverra eget mi.
+          Nunc tincidunt libero sed velit ultricies pharetra. Aliquam vitae nunc
+          eu ligula mollis ullamcorper quis nec lacus. Sed porta vel libero sit
+          amet porta.
+        </p>
+      </div>
+    </main>
+  );
+}
